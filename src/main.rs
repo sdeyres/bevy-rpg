0357c1252b595a4565505481220bf586adc86852
Sylvain DEYRES <sylvain.deyres@hardis-group.com>
2026-08-24 16:14:31 +0200

    Chapter 1 end

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+mod player;
+
+use bevy::prelude::*;
+
+use crate::player::PlayerPlugin;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::WHITE))
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            file_path: "src\\assets".into(),
+            ..default()
+        }))
+        .add_systems(Startup, setup_camera)
+        .add_plugins(PlayerPlugin)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
